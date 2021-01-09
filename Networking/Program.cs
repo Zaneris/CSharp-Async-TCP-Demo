a4b9fc5f89878538d7f08579c84a04935885b3a9
Zaneris <dstrike@gmail.com>
2021-01-08 19:10:56 -0500

    Hello Networking!

--- Networking/Program.cs
+++ Networking/Program.cs
@@ -3,7 +3,7 @@
 namespace Networking {
     class Program {
         static void Main() {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Networking!");
             var server = new Server("127.0.0.1", 13000);
             var client = new Client("127.0.0.1", 13000);
             client.SendTestFile();
